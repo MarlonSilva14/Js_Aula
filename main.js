8f6b70bcd65ba3525fec3203bc306397c0cc1f3d
MarlonSilva14 <60238493+MarlonSilva14@users.noreply.github.com>
2020-01-23 17:55:34 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+/* var nome = "Marlon Soares";
+var idade = 25;
+//alert("Bem vindo " + nome);
+alert(nome + " tem " + idade + " anos ");
+*/
+
+var lista = ["maça", "banana", "uva", "limão", "morango"];
+//lista.push() - Add um item na lista
+//lista.pop () - retira um item da lista
+console.log(lista);
